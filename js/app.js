0b31e0fbc586700fd4a66f7d20e1175225d95f08
Beth Rissman <beth.rissman@telepham.com>
2022-11-12 14:05:48 -0600

    testing

--- js/app.js
+++ js/app.js
@@ -1,13 +1,5 @@
 'use strict';
-/////////////////////////////////////
-/* Problem 1 (this is your demo that we'll solve in class)
-Write a function called sum() that takes in two numbers as arguments and then returns an array where the first element is the sum of those numbers, and the second element is a concatenated string that EXACTLY follows this example and uses the values that were input into the function:
-
-"The sum of 4 and 7 is 11."
-
-Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSum() function below and check the console to see if the test passes.*/
-
-// Write your code here
+gi
 function sum(a, b) { //eslint-disable-line
 
 }
